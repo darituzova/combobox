1d2ad1c86b96e1b63b0a5c6ffe970e33430a1af4
meshcher <meshcher.06@mail.ru>
2026-07-11 19:13:40 +0300

    gateways created

--- iiot_backend/infrastructure/database.py
+++ iiot_backend/infrastructure/database.py
@@ -6,7 +6,7 @@ def new_session_maker(psql_config: DatabaseSettings) -> async_sessionmaker[Async
     engine = create_async_engine(
         psql_config.url,
         pool_size = 15,
-        max_ovelflow = 15,
-        connect_args = {"command_tomeout": 5},
+        max_overflow = 15,
+        connect_args = {"command_timeout": 5},
     )
     return async_sessionmaker(engine, class_=AsyncSession, autoflush=False, expire_on_commit=False)
---
+++ iiot_backend/infrastructure/gateways.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import session
+from sqlalchemy.sql import text
+from application.interfaces import TelemetrySaver
+from domain.entities import TelemetryDM
+
+class TelemetryGateway(TelemetrySaver):
+    def __init__(self):
+        self._session = session
+
+    async def save(self, telemetry: TelemetryDM):
+        "Сохранение записи телеметрии"
+        query = text("""
+            INSERT INTO telemetry (
+                time, machine_id, temperature, vibration, humidity, pressure, energy_consumption
+            ) VALUES (
+                :time, :machine_id, :temperature, :vibration, :humidity, :pressure, :energy_consumption
+            )
+        """)
+        await self._session.execute(
+            statement=query,
+            parems = {
+                "time": telemetry.timestamp,
+                "machine_id": telemetry.machine_id,
+                "temperature": telemetry.temperature,
+                "vibration": telemetry.vibration,
+                "humidity": telemetry.humidity,
+                "pressure": telemetry.pressure,
+                "energy_consumption": telemetry.energy_consumption
+            }
+        )
